03f93afb01794325688fb66e8fbbb0ecc73cedc6
VicTroia-Dev <121833087+ToriCrux@users.noreply.github.com>
2023-02-06 12:08:23 -0300

    Javascript
    Repository where all Javascript language studies and projects are stored.

---
+++ CURSO EM VIDEO/AULA01/AULA01.js
@@ -0,0 +1,24 @@
+/* SERVIDOR X CLIENTE
+
+Clientes -> computadores, celulares e até mesmo televisões são clientes, pois eles consomem
+os serviços fornecidos pelos servidores.
+
+Servidores -> máquinas de alta perfomance que armazenam conteúdos.
+
+Navegadores -> pontes que fazem a conexã entre os clientes e os serivodores. Através das URLs,
+vão para a infraestrutura da internet e respondem qual exato caminho o cliente deve ser 
+desviado para chegar onde deseja.
+
+Infraestrutura da Internet -> é a responsável por encaminhar o cliente para onde ele deseja, 
+ou seja, ela é a responsável por direcionar a pesquisa para o diretório do servidor onde se 
+encontra o arquivo desejado, no caso o htlm do site em questão.
+
+WEBSITES
+
+Os websites são na grande maioria das vezes compostos por 3 tecnologias no lad do cliente:
+    - html (conteúdo)
+    - css (desing)
+    - javascript (interações, engenharia da entrega, configuração e integração dos elementos)
+
+*/
+
